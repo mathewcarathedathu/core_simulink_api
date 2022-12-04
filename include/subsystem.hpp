609fffb3b9b10c85fbfe2871a0921de53abcc6ed
Mathew Carathedathu <5600690+mathewcarathedathu@users.noreply.github.com>
2022-12-04 13:09:04 -0800

    WIP: subsystem defintion.

---
+++ include/subsystem.hpp
@@ -0,0 +1,20 @@
+// Definition of subsystem in simulink model
+
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+
+#include "block.h"
+
+namespace n_subsys
+{
+	class subsystem
+	{
+	private:
+
+	public:
+		bool add_block(n_block::block)
+	};
+}
