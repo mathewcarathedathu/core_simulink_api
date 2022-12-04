74e701cac33ac99c5852e1ab767737ab5480a3cf
Mathew Carathedathu <5600690+mathewcarathedathu@users.noreply.github.com>
2022-12-04 13:08:49 -0800

    Update block definition to introduce some helper functions

--- include/block.h
+++ include/block.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <memory>
 #include <string>
+#include <set>
 #include "data_unit.h"
 
 static const unsigned MAX_BLOCK_INPUT = 100;
@@ -19,29 +20,54 @@ enum class block_type : unsigned char
 	E_INTEG,
 	E_DIFF	
 };
-
-class block
+namespace n_block
 {
-private:
-	uint8_t num_inputs;
-	uint8_t num_outputs;
-	
-	std::shared_ptr<data_unit> inputs;
-	std::shared_ptr<data_unit> outputs;
-
-	std::string id;
-	block_type type;
-
-	block* child;
-	block* parent;
-public:
-
-	virtual void execute() = 0;
-
-	block_type get_type()
+	class block
 	{
-		return type;
-	}
+	private:
+		uint8_t num_inputs;
+		uint8_t num_outputs;
 
+		std::shared_ptr<data_unit> inputs;
+		std::shared_ptr<data_unit> outputs;
 
-};
+		std::string id;
+		std::string subsystem_id;
+		block_type type;
+
+		std::set<block*> child;
+		std::set<block*> parent;
+	public:
+
+		block()
+		{
+			subsystem_id.clear();
+		}
+
+		virtual void assign_id() = 0;
+		virtual void assign_type(block_type b_type) = 0;
+		virtual void execute() = 0;
+
+		block_type get_type()
+		{
+			return type;
+		}
+
+		std::string get_id()
+		{
+			return id;
+		}
+
+		std::set<block*> get_parents()
+		{
+			std::set<block*> parent_(parent);
+			return parent_;
+		}
+
+		std::set<block*> get_children()
+		{
+			std::set<block*> child_(child);
+			return child_;
+		}
+	};
+}
