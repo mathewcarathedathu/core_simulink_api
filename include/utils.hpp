73e5bd914a982b8115dc57caac3fd285a58bfe97
Mathew Carathedathu <5600690+mathewcarathedathu@users.noreply.github.com>
2022-12-12 03:16:47 -0800

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,10 +3,48 @@
 #include <stdint.h>
 #include <unordered_set>
 #include <set>
+#include <string>
 
 #include "func_unit.hpp" 
+#include "block.hpp"
 
 using namespace base;
+
+
+std::string get_block_type_str(components::block* block_)
+{
+
+	std::string type_str;
+
+	switch (block_->get_type())
+	{
+	case ::block_type::E_DIFF:
+		type_str =  "Differentiate";
+		break;
+
+	case ::block_type::E_GAIN:
+		type_str =  "Gain";
+		break;
+
+	case ::block_type::E_INPUT:
+		type_str =  "Input";
+		break;
+
+	case ::block_type::E_INTEG:
+		type_str =  "Integration";
+		break;
+
+	case ::block_type::E_OUTPUT:
+		type_str =  "Output";
+		break;
+
+	default:
+		type_str =  "Unassigned";
+		break;
+	}
+
+	return type_str;
+}
 
 void merge_sets(std::unordered_set<func_unit*>& target, std::set<func_unit*> source)
 {
