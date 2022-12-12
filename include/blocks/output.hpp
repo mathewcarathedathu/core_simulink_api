d56a1e75e5b0e7d29202fbecc2c9a567b94b82a1
Mathew Carathedathu <5600690+mathewcarathedathu@users.noreply.github.com>
2022-12-11 21:59:19 -0800

    Output block to print output.

---
+++ include/blocks/output.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+
+#include "block.hpp"
+
+namespace f_block
+{
+	class output_block : public components::block
+	{
+	private:
+		double prev_num;
+		int num_ip = 1;
+		int num_op = 0;
+
+
+	public:
+
+		output_block(int subsys_id = -1) :
+			block(num_ip, num_op, ::block_type::E_OUTPUT, subsys_id),
+			prev_num(0)
+		{};
+
+		bool execute()
+		{
+
+			auto a = inputs[0]->data;
+			auto b = inputs[0]->update_num;
+			
+			std::cout << inputs[0]->data << ", " << inputs[0]->update_num << std::endl;
+
+			return true;
+		}
+	};
+}
